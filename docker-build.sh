e35d53ca6d1b788cd995fbfe7e266487acd0fa5f
Phil <phil.willis@nike.com>
2023-03-08 12:56:07 -0800

    docker run example no background

--- docker-build.sh
+++ docker-build.sh
@@ -5,4 +5,4 @@ unzip -d app -o app.zip
 popd
 
 docker build --tag "snack-attack-api" .
-# docker run -d --name snack-attack --rm  -p 3000:3000 -t snack-attack-api
+# docker run --name snack-attack --rm  -p 3000:3000 -t snack-attack-api
