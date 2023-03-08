2137c5de5c48eb673c4f50b284b527011e152346
Phil <phil.willis@nike.com>
2023-03-08 15:00:51 -0800

    added config service to deal with env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
 import { QuotesModule } from './quotes/quotes.module'
 
+const EnvModule = ConfigModule.forRoot({
+  envFilePath: ['.env.local', '.env'],
+})
+
 @Module({
-  imports: [QuotesModule],
+  imports: [EnvModule, QuotesModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
-import * as dotenv from 'dotenv'
 
 import { AppModule } from './app.module'
 
-dotenv.config()
-
-const PORT = process.env.PORT || 3000
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+
+  const PORT = configService.get('PORT') ?? 3000
   console.log(`⚡ The magic happens at: http://localhost:${PORT}`)
   await app.listen(PORT)
 }
