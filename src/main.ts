f943f3c421ce1a6ba13b6d07514588b329ebbe78
Phil <phil.willis@nike.com>
2023-03-08 11:12:49 -0800

    init, linting, zip build, dotenv

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core'
+import * as dotenv from 'dotenv'
+
+import { AppModule } from './app.module'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(PORT)
+}
+bootstrap()
